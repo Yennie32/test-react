21e94045379a71b17ed397c865a6c4b2630d01e0
Yennie <yennielake@gmail.com>
2025-04-25 17:18:52 +0200

    working nav bar

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { main } from "framer-motion/client";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Tech from "./components/Tech";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <>
+      <div className="fixed -z-10 min-h-screen w-full
+      [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+      </div>
+      <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
+        <Navbar />
+        <Hero />
+        <Tech />
+        <Projects />
+        <Contact />
+      </main>
+    </>
+  );
+}
+
+export default App;
